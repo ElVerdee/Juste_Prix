5ab0efc5e03d371c9f948a6d12dc05e7a4699bd6
Wasil <warh63807@eleve.isep.fr>
2024-08-27 13:47:06 +0200

    first commit

---
+++ Juste_Prix.py
@@ -0,0 +1,6 @@
+import random
+print("Quelle est le prix de l'objet X...?")
+cible = random.randint(1,10)
+for k in range (0,6) :
+    prop = int(input(f"Votre proposition entre 1 et 10 ({5-k} essais restants) :"))
+    if 
---
+++ README.md
@@ -0,0 +1,1 @@
+# Juste_Prix
