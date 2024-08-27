9618c194ea6c0ac69df204c6cb5bf95c182e4c82
Wasil <warh63807@eleve.isep.fr>
2024-08-27 16:38:56 +0200

    Pour faire des test unitaires

--- Juste_Prix.py
+++ Juste_Prix.py
@@ -1,19 +1,36 @@
 import random
-print("Quelle est le prix de l'objet X...?")
-cible = random.randint(1,10)
-#print(cible)
-for k in range (6) :
-    if k > 4 :
-        print("Vous avez perdu!!")
-        break
-    prop = int(input(f"Votre proposition entre 1 et 10 ({5-k} essais restants) :"))
-    if prop <1 or prop > 10 :
+
+def verif(prop,cible):
+    if prop < 1 or prop > 10:
         print("Erreur ! Il faut choisir un prix entre 1 et 10")
-    if prop > cible :
+        return "continuer"
+    if prop > cible:
         print("Trop grand")
-    elif prop < cible :
+        return "continuer"
+    elif prop < cible:
         print("Trop bas")
-    else :
+        return "continuer"
+    else:
         print("Bravo!!!")
-        break
-print("Fin du jeu")
+        return "bravo"
+
+def juste_prix() :
+
+    print("Quelle est le prix de l'objet X...?")
+    cible = random.randint(1,10)
+    #print(cible)
+    for k in range (6) :
+        if k > 4 :
+            print("Vous avez perdu!!")
+            break
+        try :
+            prop = int(input(f"Votre proposition entre 1 et 10 ({5-k} essais restants) :"))
+        except ValueError:
+            print("Erreur: Il faut choisir un entier")
+            continue
+
+        if verif(prop, cible) == "bravo":
+            break
+    print("Fin du jeu")
+if __name__ == "__main__" :
+    juste_prix()
