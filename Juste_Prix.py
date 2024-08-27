342ef124a6ee1a5685a0b0fd3bbe2c55884d8772
Wasil <warh63807@eleve.isep.fr>
2024-08-27 14:53:31 +0200

    PETITE MODIF

--- Juste_Prix.py
+++ Juste_Prix.py
@@ -1,12 +1,14 @@
 import random
 print("Quelle est le prix de l'objet X...?")
 cible = random.randint(1,10)
-print(cible)
-for k in range (0,6) :
+#print(cible)
+for k in range (6) :
     if k > 4 :
-        print("Vous avez perdu")
+        print("Vous avez perdu!!")
         break
     prop = int(input(f"Votre proposition entre 1 et 10 ({5-k} essais restants) :"))
+    if prop <1 or prop > 10 :
+        print("Erreur ! Il faut choisir un prix entre 1 et 10")
     if prop > cible :
         print("Trop grand")
     elif prop < cible :
@@ -14,3 +16,4 @@ for k in range (0,6) :
     else :
         print("Bravo!!!")
         break
+print("Fin du jeu")
